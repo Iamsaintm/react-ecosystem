f02c2d93a580cfdaf34705a1f9fa70491f96f689
Iamsaintm <p.leelayuthto@hotmail.com>
2023-09-08 11:30:24 +0700

    finish:app

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,38 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { createContext, useContext, useState } from "react";
+
+// AuthContext
+const AuthContext = createContext();
+
+function AuthContextProvider(props) {
+  const [isAuth, setIsAuth] = useState(false);
+  const handleAuth = () => {
+    setIsAuth(!isAuth);
+  };
+  const shareObj = { toggleAuth: handleAuth, isAuth: isAuth };
+  return (
+    <AuthContext.Provider value={shareObj}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+}
+
+function App() {
+  const auth = useContext(AuthContext);
+
+  return (
+    <div className="App">
+      <h3>Welocome.. {auth.isAuth ? "Guest" : "User"}</h3>
+      <button onClick={auth.toggleAuth}>
+        {auth.isAuth ? "Login" : "Logout"}
+      </button>
+    </div>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <div>Vite React Template</div>
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>
 );
